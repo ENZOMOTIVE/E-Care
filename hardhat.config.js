9a0d6ac8c206cc38ca94c5433d45b69bca1c0bfb
ENZOMOTIVE <91072069+ENZOMOTIVE@users.noreply.github.com>
2024-05-27 11:59:30 +0530

    network_commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 const privateKeys = process.env.PRIVATE_KEYS || "";
-const goerliApiKey = process.env.GOERLI_API_KEY;
-const mumbaiApiKey = process.env.MUMBAI_API_KEY;
+const BNB = process.env.BNB_RPC;
+
 module.exports = {
   solidity: "0.8.18",
   networks: {
     localhost: {},
-    goerli: {
-      url: goerliApiKey,
-      accounts: privateKeys.split(","),
-    },
-    mumbai: {
-      url: mumbaiApiKey,
+    BNB: {
+      url: BNB,
       accounts: privateKeys.split(","),
     },
   },
