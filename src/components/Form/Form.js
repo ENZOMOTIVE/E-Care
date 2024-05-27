d72671e9a78777bbd5c1c80f3cf53b3799a43cc0
ENZOMOTIVE <91072069+ENZOMOTIVE@users.noreply.github.com>
2024-05-27 14:59:39 +0530

    photo upload

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,14 +2,22 @@ import React, { useState } from "react";
 import "./form.css";
 import { submitRecord } from "../../store/interactions";
 import { useDispatch, useSelector } from "react-redux";
-
+import PinataUpload from "./IPFS"; // Import the PinataUpload component
 
 const Form = () => {
   const provider = useSelector((state) => state.provider.connection);
   const medical = useSelector((state) => state.medical.contract);
-  const account=useSelector((state)=>state.provider.account);
+  const account = useSelector((state) => state.provider.account);
   const dispatch = useDispatch();
-  const submitHandler = (e) => {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [gender, setGender] = useState('');
+  const [bloodType, setBloodType] = useState('');
+  const [allergies, setAllergies] = useState('');
+  const [diagnosis, setDiagnosis] = useState('');
+  const [treatment, setTreatment] = useState('');
+
+  const submitHandler = async (e) => {
     e.preventDefault();
     submitRecord(
       name,
@@ -23,110 +31,118 @@ const Form = () => {
       medical,
       dispatch
     );
-    setName(0);
-    setAge(0);
-    setGender(0);
-    setBloodType(0);
-    setAllergies(0);
-    setDiagnosis(0);
-    setTreatment(0);
+    setName('');
+    setAge('');
+    setGender('');
+    setBloodType('');
+    setAllergies('');
+    setDiagnosis('');
+    setTreatment('');
   };
-  const [name, setName] = useState(0);
-  const [age, setAge] = useState(0);
-  const [gender, setGender] = useState(0);
-  const [bloodType, setBloodType] = useState(0);
-  const [allergies, setAllergies] = useState(0);
-  const [diagnosis, setDiagnosis] = useState(0);
-  const [treatment, setTreatment] = useState(0);
 
   return (
     <div className="login-container">
       <h1></h1>
-    {account?( <form onSubmit={submitHandler}>
-        <h1>Patient Details</h1>
+      {account ? (
+        <form onSubmit={submitHandler}>
+          <h1>Patient Details</h1>
 
-        <label htmlFor="name">Patient Name:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={name === 0 ? "" : name}
-          onChange={(e) => setName(e.target.value)}
-          required
-          placeholder="Enter your name"
-        />
+          {/* Patient Name */}
+          <label htmlFor="name">Patient Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+            placeholder="Enter your name"
+          />
 
-        <label htmlFor="age">Age:</label>
-        <input
-          type="number"
-          id="age"
-          name="age"
-          required
-          placeholder="Enter Age"
-          value={age === 0 ? "" : age}
-          onChange={(e) => setAge(e.target.value)}
-        />
+          {/* Age */}
+          <label htmlFor="age">Age:</label>
+          <input
+            type="number"
+            id="age"
+            name="age"
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+            required
+            placeholder="Enter Age"
+          />
 
-        <label htmlFor="gender">Gender:</label>
-        <select
-          id="gender"
-          name="gender"
-          required
-          onChange={(e) => setGender(e.target.value)}
-          value={gender === 0 ? "" : gender}
-        >
-          <option value="">Select Gender</option>
-          <option value="Male">Male</option>
-          <option value="Female">Female</option>
-          <option value="Other">Other</option>
-        </select>
+          {/* Gender */}
+          <label htmlFor="gender">Gender:</label>
+          <select
+            id="gender"
+            name="gender"
+            value={gender}
+            onChange={(e) => setGender(e.target.value)}
+            required
+          >
+            <option value="">Select Gender</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+            <option value="Other">Other</option>
+          </select>
 
-        <label htmlFor="address">Blood type:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          required
-          placeholder="B positive"
-          value={bloodType === 0 ? "" : bloodType}
-          onChange={(e) => setBloodType(e.target.value)}
-        />
-        <label htmlFor="address">Allergies:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          required
-          placeholder="Pollen allergy"
-          value={allergies === 0 ? "" : allergies}
-          onChange={(e) => setAllergies(e.target.value)}
-        />
-        <label htmlFor="address">Diagnosis:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          required
-          placeholder="Osteoporosis"
-          value={diagnosis === 0 ? "" : diagnosis}
-          onChange={(e) => setDiagnosis(e.target.value)}
-        />
-        <label htmlFor="address">Treatment:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          required
-          placeholder="Surgery"
-          value={treatment === 0 ? "" : treatment}
-          onChange={(e) => setTreatment(e.target.value)}
-        />
+          {/* Blood Type */}
+          <label htmlFor="bloodType">Blood Type:</label>
+          <input
+            type="text"
+            id="bloodType"
+            name="bloodType"
+            value={bloodType}
+            onChange={(e) => setBloodType(e.target.value)}
+            required
+            placeholder="Enter Blood Type"
+          />
 
-        <input type="submit" value="Submit" />
-      </form>):(
+          {/* Allergies */}
+          <label htmlFor="allergies">Allergies:</label>
+          <input
+            type="text"
+            id="allergies"
+            name="allergies"
+            value={allergies}
+            onChange={(e) => setAllergies(e.target.value)}
+            required
+            placeholder="Enter Allergies"
+          />
+
+          {/* Diagnosis */}
+          <label htmlFor="diagnosis">Diagnosis:</label>
+          <input
+            type="text"
+            id="diagnosis"
+            name="diagnosis"
+            value={diagnosis}
+            onChange={(e) => setDiagnosis(e.target.value)}
+            required
+            placeholder="Enter Diagnosis"
+          />
+
+          {/* Treatment */}
+          <label htmlFor="treatment">Treatment:</label>
+          <input
+            type="text"
+            id="treatment"
+            name="treatment"
+            value={treatment}
+            onChange={(e) => setTreatment(e.target.value)}
+            required
+            placeholder="Enter Treatment"
+          />
+
+          {/* Pinata Upload Component */}
+          <PinataUpload />
+
+          {/* Submit Button */}
+          <input type="submit" value="Submit" />
+        </form>
+      ) : (
         <h1>Connect the account</h1>
       )}
-     
     </div>
   );
 };
